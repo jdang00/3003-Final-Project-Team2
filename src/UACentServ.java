a7edc20cb8146aa05559c3720dc703bb7dce96a5
doyle404 <101595695+doyle404@users.noreply.github.com>
2023-12-07 02:27:37 -0600

    Implementing javadoc

--- src/UACentServ.java
+++ src/UACentServ.java
@@ -6,6 +6,15 @@ import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
+
+/*
+ * @author Justin Dang
+ * @author Doyle McHaffie
+ * @author Morgan
+ * <p>
+ * The UACentServ is the central server for this project, and handles the logger, client handler, and load balancing
+ * </p>
+ */
 public class UACentServ{
     public static final int port = 35000;
     private static final String logFile = "serverLog.txt";
@@ -16,10 +25,16 @@ public class UACentServ{
 
     static private int numFitServ;
 
-    //Setting up our logger
+    /*
+     * <p>
+     * The setupLogger() method initializes the logger for the central server and places the logs on a text file
+     * </p>
+     * @param logFile The logFile that all server information goes to which is serverLog.txt
+     * @param logger
+     */
     public void setupLogger() {
         try {
-            logger = Logger.getLogger(UAServer.class.getName());
+            logger = Logger.getLogger(UACentServ.class.getName());
             FileHandler fh = new FileHandler(logFile, true);
             fh.setFormatter(new SimpleFormatter());
             logger.addHandler(fh);
@@ -29,7 +44,14 @@ public class UACentServ{
         }
     }
 
-    //Creates our Server
+    /*
+     * <p>
+     * This UACentServ() constructor creates the logger and bounds two sockets to two different ports
+     * </p>
+     * @param setupLogger() in this instance is created for the server to start logging 
+     * @param fs is a socket bound to port 35555
+     * @param ss is a socket bound to port 35000 but is dynamic 
+     */
     public UACentServ() {
         try {
             setupLogger();
@@ -46,7 +68,9 @@ public class UACentServ{
     }
 
 
-    //This is the client handler and is set up to handle inputs
+    /*
+     * Client handler method
+     */
     public void clientHandler(Socket cs) {
 
         new Thread(() -> fittingRoomServerHandler(FitRoomServersList.get(loadBalancer()), loadBalancer())).start();
@@ -55,6 +79,9 @@ public class UACentServ{
     }
 
 
+    /*
+     * fittingRoomServerHandler takes in a socket and server number and currently handles each client incoming
+     */
     public void fittingRoomServerHandler(Socket cs, int servNum){
 
         try{
@@ -73,6 +100,14 @@ public class UACentServ{
     int clientCounter = 0;
 
     //This start method pushes the client connection and sends it to the client() method being passed a socket
+    /*
+     * <p>
+     * The start method accepts a connection for a server and adds it to a =n ArrayList of servers
+     * The start method additionally accepts client connections and starts the thread with the client 
+     * </p>
+     * @param FitRoomServerList an array list of servers to manage clients/customers
+     * @param clientServ an instance of the client that is pushed to boot up
+     */
     public void start() {
 
         try {
@@ -106,6 +141,11 @@ public class UACentServ{
         }
     }
 
+    /*
+     * <p>
+     * The loadBalancer() method returns an integer and handles the load balancing for which server a client will be connected to 
+     * </p>
+     */
     public int loadBalancer(){
 
         if(roundRobinCounter == FitRoomServersList.size()){
@@ -119,7 +159,13 @@ public class UACentServ{
 
 
 
-    //Simply starts the server
+    /*
+     * <p>
+     * The main() method establishes the number of fitting room servers and starts the central server
+     * </p>
+     * @param numFitServ The specified amount of servers 
+     * @param server the creation of the central server
+     */
     public static void main(String[] args) {
         numFitServ = 3;
         UACentServ server = new UACentServ();
@@ -130,6 +176,3 @@ public class UACentServ{
 
 
 }
-
-
-
