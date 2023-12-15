7f11a9535ca2389a6d5b13e044481c6f334aa5ac
doyle404 <101595695+doyle404@users.noreply.github.com>
2023-12-15 12:03:09 -0600

    Java Docs

--- src/UAClient.java
+++ src/UAClient.java
@@ -5,7 +5,12 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
+/**
+ * The UAClient class just obtains our user input stream and initalizes the amount of threads to be ran through the FittingRoomServer
+ * @author Justin Dang
+ * @author Doyle McHaffie
+ * @author Morgan Ballard
+ */
 public class UAClient {
     //Creating our Socket,Host, and Port
     private static final String host = "localhost";
@@ -14,6 +19,12 @@ public class UAClient {
 
 
     //Creating the Client and obtaining the users input stream
+
+    /**
+     * the UAClient() method will bind the socket to a host and port 
+     * @param host will be our local host
+     * @param port is the default assigned port of 35000
+     */
     public UAClient() {
         try {
             Socket cs = new Socket(host, port);
@@ -24,10 +35,12 @@ public class UAClient {
 
 
 
-
+    /**
+     * the main() method will take one command line input of the number of clients to be ran through UAFittingRoomServer and starts those threads up. 
+     */
     public static void main(String[] args) {
-
-        for(int i = 0; i < 15; i++){
+        int nums = Integer.parseInt(args[0]);
+        for(int i = 0; i < nums; i++){
             UAClient client = new UAClient();
             try {
                 Thread.sleep(new Random().nextInt(1000));
